a09c019f050e03afa9d196005b44f4ad34b487e8
yuan114514 <126371453+yuan114514@users.noreply.github.com>
2023-03-26 19:11:21 +0800

    Update baiscriptnb666888.lua

--- baiscriptnb666888.lua
+++ baiscriptnb666888.lua
@@ -9,20 +9,38 @@ end)
 -- Inside Handler (LocalScript) --
  
 local RunService = game:GetService("RunService")
+local PingRemote = script.GetPing
  
 local FPSCounter = script.Parent.Holder.FPS
+local PingCounter = script.Parent.Holder.Ping
  
 local Colors = {
     Good = Color3.fromRGB(0, 255, 0),
     Normal = Color3.fromRGB(255, 255, 0),
     Bad = Color3.fromRGB(255, 0, 0)
 }
-
+ 
+function GetPing()
+    local Send = tick()
+    local Ping = nil
+ 
+    PingRemote:FireServer()
+ 
+    local Receive; Receive = PingRemote.OnClientEvent:Connect(function()
+        Ping = tick() - Send 
+    end)
+ 
+    wait(1)
+ 
+    Receive:Disconnect()
+ 
+    return Ping or 999
+end
  
 RunService.RenderStepped:Connect(function(TimeBetween)
     local FPS = math.floor(1 / TimeBetween)
  
-    FPSCounter.Text = FPS
+    FPSCounter.Text = tostring(FPS)
  
     if FPS >= 50 then
         FPSCounter.TextColor3 = Colors.Good
@@ -35,7 +53,26 @@ RunService.RenderStepped:Connect(function(TimeBetween)
  
     end
 end)
-
+ 
+local PingThread = coroutine.wrap(function()
+    while wait() do
+        local Ping = tonumber(string.format("%.3f", GetPing() * 1000))
+        PingCounter.Text = (math.floor(Ping)).." ms"
+ 
+        if Ping <= 100 then
+            PingCounter.TextColor3 = Colors.Good
+ 
+        elseif Ping > 199  then
+            PingCounter.TextColor3 = Colors.Normal
+ 
+        elseif Ping > 900 then
+            PingCounter.TextColor3 = Colors.Normal
+ 
+        end
+    end
+end)
+ 
+PingThread()
 
 restorebutton = Instance.new("ScreenGui",game:GetService("CoreGui"))
 bai = Instance.new("TextButton",restorebutton)
@@ -46,7 +83,7 @@ bai.Position = UDim2.new(1,-700,1,-23)
 bai.Size = UDim2.new(0,100,0,20)
 bai.Font = "SourceSans"
 bai.FontSize = "Size18"
-bai.Text = "白 ● 防伪"
+bai.Text = "你的注入器："..identifyexecutor()"
 bai.TextColor3 = Color3.new(255/255,255/255,255/255)
 
 if game.PlaceId == 189707 then
